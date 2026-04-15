179f979f17834594c31b468219f7b836796f50da
impossibleiman <iman6@hotmail.co.uk>
2026-04-15 03:26:31 +0100

    Add modular button listener to CommandManager for handling button interactions and get permissions from command

--- minecraft-society-bot/src/main/java/com/example/CommandManager.java
+++ minecraft-society-bot/src/main/java/com/example/CommandManager.java
@@ -1,10 +1,12 @@
 package com.example;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,22 +23,39 @@ public class CommandManager extends ListenerAdapter {
 
     @Override
     public void onReady(ReadyEvent event) {
-        // This tells Discord what commands our bot has when it turns on
         List<CommandData> commandData = new ArrayList<>();
         for (ICommand command : commands.values()) {
+            // Build the command and set the permissions dynamically from the command class
             commandData.add(Commands.slash(command.getName(), command.getDescription())
-                    .addOptions(command.getOptions()));
+                    .addOptions(command.getOptions())
+                    .setDefaultPermissions(command.getPermission()));
         }
         event.getJDA().updateCommands().addCommands(commandData).queue();
-        System.out.println("Slash Commands synced to Discord!");
+        System.out.println("All Slash Commands have been synced to Discord!");
     }
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        // This routes the command to the right file
         ICommand command = commands.get(event.getName());
         if (command != null) {
             command.execute(event);
+        }
+    }
+
+ // --- NEW: Modular Button Listener ---
+    @Override
+    public void onButtonInteraction(ButtonInteractionEvent event) {
+        // We expect button IDs to look like "commandName:actionData"
+        String[] parts = event.getComponentId().split(":", 2); 
+        
+        if (parts.length > 0) {
+            String commandName = parts[0]; // Extracts the prefix (e.g., "event")
+            
+            // Find the command file that matches the prefix and route the click to it!
+            ICommand command = commands.get(commandName);
+            if (command != null) {
+                command.onButtonInteraction(event);
+            }
         }
     }
 }
